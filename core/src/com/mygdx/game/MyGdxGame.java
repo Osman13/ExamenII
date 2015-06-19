5fe6c307f1cc07374f88e4d2b3317c645a820c10
Osman Oliva <osmanoliva13@unitec.edu>
2015-06-18 21:50:27 -0600

    Falta el Random
    Random

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -15,132 +15,154 @@ import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 public class MyGdxGame extends ApplicationAdapter {
 	Random ran = new Random();
 	SpriteBatch batch;
-	Texture img1,img2,img3,cpu1,cpu2,cpu3;
-	boolean visible1=false;
-	boolean visible2=false;
-	boolean visible3=false;
-	boolean visible4=false;
-	boolean visible5=false;
-	boolean visible6=false;
-	int computer_choice = ran.nextInt(2)+1;
+	Texture background,tijera1,piedra1,papel1,piedra2,papel2,tijera2;
+	Texture ganador,perdedor,empate;
+
+	boolean visibletijera1=false;
+	boolean visiblepiedra1=false;
+	boolean visiblepapel1=false;
+	boolean visiblepiedra2=false;
+	boolean visiblepapel2=false;
+	boolean visibletijera2=false;
+	boolean visibleganador = false;
+	boolean visibleperdedor = false;
+	boolean visibleempate = false;
+	int computer_choice = ran.nextInt(3)+1;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img1 = new Texture("tijera.jpg");
-		img2 = new Texture("piedra.jpg");
-		img3 = new Texture("papel.jpg");
-		cpu1 = new Texture("piedra.jpg");
-		cpu2 = new Texture("papel.jpg");
-		cpu3 = new Texture("tijera.jpg");
+		background = new Texture("background.jpg");
+		tijera1 = new Texture("tijera.png");
+		piedra1 = new Texture("piedra.png");
+		papel1 = new Texture("papel.png");
+		piedra2 = new Texture("piedra.png");
+		papel2 = new Texture("papel.png");
+		tijera2 = new Texture("tijera.png");
+		ganador = new Texture("ganador.png");
+		perdedor = new Texture("perdedor.png");
+		empate = new Texture("empatados.png");
 
 	}
 
 	@Override
 	public void render() {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 0);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		if(visible1) {
-			batch.draw(img1, 0, 0);
-		}
-		if(visible2) {
-			batch.draw(img2, 0, 0);
-		}
-		if(visible3) {
-			batch.draw(img3, 0, 0);
-		}
-		if(visible4) {
-			batch.draw(cpu1, 350, 250);
-		}
-		if(visible5) {
-			batch.draw(cpu2, 350, 250);
-		}
-		if(visible6) {
-			batch.draw(cpu3, 350, 250);
-		}
-		batch.end();
+		batch.draw(background,150, 0);
+		if(visibletijera1)
+			batch.draw(tijera1, 0, 0);
+
+		if(visiblepiedra1)
+			batch.draw(piedra1, 0, 0);
+
+		if(visiblepapel1)
+			batch.draw(papel1, 0, 0);
+
+		if(visiblepiedra2)
+			batch.draw(piedra2, 350, 250);
+
+		if(visiblepapel2)
+			batch.draw(papel2, 350, 250);
+
+		if(visibletijera2)
+			batch.draw(tijera2, 350, 250);
+
+		if(visibleganador)
+			batch.draw(ganador, 160, 150);
+
+		if(visibleperdedor)
+			batch.draw(perdedor, 160, 150);
+
+		if(visibleempate)
+			batch.draw(empate, 160, 150);
 
 
 		if(Gdx.input.isKeyPressed(Input.Keys.A)){
-			visible1=true;
+			visibletijera1=true;
 			switch (computer_choice)
 			{
 				case 1:
-					visible4=true;
+					visiblepiedra2=true;
 					break;
 
 				case 2:
-					visible5=true;
+					visiblepapel2=true;
 					break;
 
 				case 3:
-					visible6=true;
+					visibletijera2=true;
 					break;
 			}
 			if(computer_choice == 1){
-				System.out.println("El Ganador fue la computadora!");
+				visibleperdedor = true;
 			} else if (computer_choice == 2){
-				System.out.println("El Ganador fuiste tu!");
+				visibleganador = true;
 			} else if (computer_choice == 3){
-				System.out.println("Empatados!");
+				visibleempate = true;
 			}
 
 		}
 		if(Gdx.input.isKeyPressed(Input.Keys.S)){
-			visible2=true;
+			visiblepiedra1=true;
 			switch (computer_choice)
 			{
 				case 1:
-					visible4=true;
+					visiblepiedra2=true;
 					break;
 
 				case 2:
-					visible5=true;
+					visiblepapel2=true;
 					break;
 
 				case 3:
-					visible6=true;
+					visibletijera2=true;
 					break;
 			}
 			if(computer_choice == 1){
-				System.out.println("Empatados!");
+				visibleempate = true;
 			} else if (computer_choice == 2){
-				System.out.println("El Ganador fue la computadora!");
+				visibleperdedor = true;
 			} else if (computer_choice == 3){
-				System.out.println("El Ganador fuiste tu!");
+				visibleganador = true;
 			}
 		}
 		if (Gdx.input.isKeyPressed(Input.Keys.D)){
-			visible3=true;
+			visiblepapel1=true;
 			switch (computer_choice) {
 				case 1:
-					visible4=true;
+					visiblepiedra2=true;
 					break;
 
 				case 2:
-					visible5 = true;
+					visiblepapel2=true;
 					break;
 
 				case 3:
-					visible6=true;
+					visibletijera2=true;
 					break;
 			}
 			if(computer_choice == 1){
-				System.out.println("El Ganador fuiste tu!");
+				visibleganador = true;
 			} else if (computer_choice == 2){
-				System.out.println("Empatados!");
+				visibleempate = true;
 			} else if (computer_choice == 3){
-				System.out.println("El Ganador fue la computadora!");
+				visibleperdedor = true;
 			}
 		}
 		if(Gdx.input.isKeyPressed(Input.Keys.R)) {
-			visible1=false;
-			visible2=false;
-			visible3=false;
-			visible4=false;
-			visible5=false;
-			visible6=false;
+			visibletijera1=false;
+			visiblepiedra1=false;
+			visiblepapel1=false;
+			visiblepiedra2=false;
+			visiblepapel2=false;
+			visibletijera2=false;
+			visibleganador=false;
+			visibleperdedor=false;
+			visibleempate=false;
 		}
+
+		batch.end();
 	}
 }
